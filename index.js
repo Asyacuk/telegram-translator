6b5c7371bc5f6e710fd46df16845d1674e5e47d0
Asyacuk <123331796+Asyacuk@users.noreply.github.com>
2023-02-02 21:03:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+const { Telegraf } = require('telegraf')
+const translator = require('translation-google')
+const config = require ('./config.json')
+
+const bot = new Telegraf(config.BOT_TOKEN)
+
+
+
+
+bot.start((ctx) => {
+    ctx.reply(`<b>Merhaba @${ctx.from.username}, bana çevirmek istediğin bir metin gönder senin için türkçeye çevireyim.</b>`, {parse_mode: 'HTML'});
+    });
+
+
+bot.on('text', async (ctx) => {
+    const text = ctx.update.message.text;
+    const translation = await translator(text, {to: 'tr'})
+    ctx.reply(`*${translation.text}*`, {parse_mode: 'Markdown',
+    reply_to_message_id: ctx.update.message.message_id});
+});
+
+
+bot.on('photo', async (ctx) => {
+    const text = ctx.update.message.caption;
+    if(text) {
+        const translation = await translator(text, {to: 'tr'});
+        ctx.replyWithPhoto(ctx.update.message.photo[0].file_id, { 
+            caption: `*${translation.text}*`, 
+            parse_mode: 'Markdown', reply_to_message_id: ctx.update.message.message_id
+        });
+        
+    }
+    else {
+        ctx.reply("Anlayamadım. Lütfen tekrar edin.")
+    }
+})
+
+
+bot.on('video', async (ctx) => {
+    const text = ctx.update.message.caption;
+    if(text) {
+        const translation = await translator(text, {to: 'tr'});
+        ctx.replyWithVideo(ctx.update.message.video.file_id, { 
+            caption: `*${translation.text}*`,
+            parse_mode: 'Markdown', reply_to_message_id: ctx.update.message.message_id
+        });
+    }
+    else {
+        ctx.reply("Lütfen video altına metin ekleyiniz.")
+    }
+})
+
+
+bot.on('voice', async (ctx) => {
+    const text = ctx.update.message.caption;
+    if(text) {
+        const translation = await translator(text, {to: 'tr'});
+        ctx.replyWithVoice(ctx.update.message.voice.file_id, { 
+            caption: `*${translation.text}*`,
+            parse_mode: 'Markdown', reply_to_message_id: ctx.update.message.message_id
+        });
+    }
+    else {
+        ctx.reply("Lütfen ses dosyasının altına metin ekleyiniz.")
+    }
+})
+
+
+bot.on('audio', async (ctx) => {
+    const text = ctx.update.message.caption;
+    if(text) {
+        const translation = await translator(text, {to: 'tr'});
+        ctx.replyWithAudio(ctx.update.message.audio.file_id, {
+            caption: `*${translation.text}*`,
+            parse_mode: 'Markdown', reply_to_message_id: ctx.update.message.message_id
+            });
+    }
+    else {
+        ctx.reply("Lütfen ses dosyasına metin ekleyiniz.")
+    }
+})
+
+
+bot.launch()
